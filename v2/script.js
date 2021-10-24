abc06d9c308a57dbf02370345c0d41aaa186608c
kmddr <86179894+kmddr@users.noreply.github.com>
2021-10-24 17:27:17 -0400

    update

--- v2/script.js
+++ v2/script.js
@@ -1,30 +1,25 @@
-var header = document.getElementById("header");
 const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-window.onscroll = function() {
-  (window.pageYOffset > header.offsetTop) ? header.classList.add("sticky") : header.classList.remove("sticky");
-};
+
 function OpenMobileMenu() {
   var x = document.getElementsByClassName("mobile-links")[0];
   var y = document.getElementsByClassName("hamburger")[0];
   if (x.style.display === "block") {
-    x.classList.remove("fadein");
-    x.classList.add("fadeout");
+    x.classList.remove("mlin");
+    x.classList.add("mlout");
     setTimeout(function(){
       x.style.display = "none";
-      x.classList.remove("fadeout");
+      x.classList.remove("mlout");
     },300);
     y.classList.remove("is-active");
   } else {
-    x.classList.add("fadein");
+    x.classList.add("mlin");
     x.style.display = "block";
     y.classList.add("is-active");
   }
 }
-function CheckForMobileMenu() {
+window.onresize = function () {
   var x = document.getElementsByClassName("mobile-links")[0];
   if (vw > 1147 && x.style.display === "block") {
     x.style.display = "none";
   }
-  console.log("running")
 }
-window.onresize = CheckForMobileMenu;
